1a76db3eaf7a62bb264d3e8eaf53e0d373fb69a0
Vekkumasa <ylirasku@gmail.com>
2020-04-22 11:48:42 +0300

    heroku

--- application/auth/models.py
+++ application/auth/models.py
@@ -58,10 +58,10 @@ class User(db.Model):
 
     @staticmethod
     def find_reservations(kampaaja_id):
-        stmt = text(' SELECT "Varaus".id, "Aika".pvm, "Aika".aika_h, "Aika".aika_min, "Asiakas".firstName FROM "Kampaaja" '
+        stmt = text(' SELECT "Varaus".id, "Aika".pvm, "Aika".aika_h, "Aika".aika_min, "Asiakas"."firstName" FROM "Kampaaja" '
                     ' JOIN "Varaus" ON "Varaus".kampaaja_id = "Kampaaja".id '
                     ' JOIN "Aika" ON "Aika".id = "Varaus".aika_id '
-                    ' JOIN "Asiakas" ON "Asiakas".phoneNumber = "Varaus".asiakas_id '
+                    ' JOIN "Asiakas" ON "Asiakas"."phoneNumber" = "Varaus".asiakas_id '
                     ' WHERE ("Kampaaja".id = %s) ' % (kampaaja_id))
         res = db.engine.execute(stmt)
 
